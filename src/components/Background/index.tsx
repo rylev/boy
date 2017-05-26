c9d5b48f60dde31fcd3a8c4380e5755ef548b11b
Ryan Levick <ryan.levick@gmail.com>
2017-05-26 12:03:20 +0200

    Naming improvements

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -36,31 +36,32 @@ class Background extends React.Component<Props, State> {
         const widthInTiles = 32 
         const heightInTiles = 32
 
-        const tileWidth = 8
-        const tileHeight = 8
+        const tileWidthInPixels = 8
+        const tileHeightInPixels = 8
 
         const valuesPerPixel = 4
 
-        const rowWidth = tileWidth * widthInTiles * valuesPerPixel
+        const rowWidthInCanvasValues = tileWidthInPixels * widthInTiles * valuesPerPixel
 
-        const canvasData: Uint8Array = new Uint8Array(widthInTiles * heightInTiles * tileHeight * tileWidth * valuesPerPixel).fill(0)
+        const canvasDataLength = widthInTiles * heightInTiles * tileHeightInPixels * tileWidthInPixels * valuesPerPixel
+        const canvasData: Uint8Array = new Uint8Array(canvasDataLength).fill(0)
         const tiles: TileValue[][][] = Array.from(background).map((byte: number) => tileSet[byte])
 
         tiles.forEach((tile, tileIndex) => {
             const tileRow = Math.trunc(tileIndex / heightInTiles)
             const tileColumn = tileIndex % widthInTiles
             tile.forEach((innerRow, innerRowIndex) => {
-                const pixelRow = (tileRow * tileHeight) + innerRowIndex
-                const beginningOfCanvasRow = pixelRow * rowWidth
-                const beginningOfColumn = tileColumn * tileWidth 
+                const pixelRowIndex = (tileRow * tileHeightInPixels) + innerRowIndex
+                const beginningOfCanvasRow = pixelRowIndex * rowWidthInCanvasValues
+                const beginningOfColumn = tileColumn * tileWidthInPixels
                 let index = beginningOfCanvasRow + (beginningOfColumn * valuesPerPixel)
 
                 innerRow.forEach((pixel, i) => {
-                    const pixelColumn = beginningOfColumn + i
-                    const onScreenBorderX = (gpu.scrollY === pixelRow || pixelRow === gpu.scrollY + 144) && 
-                        (pixelColumn > gpu.scrollX && pixelColumn < gpu.scrollX + 160)
-                    const onScreenBorderY = (gpu.scrollX === pixelColumn || gpu.scrollX + 160 === pixelColumn) && 
-                        (pixelRow > gpu.scrollY && pixelRow < gpu.scrollY + 144)
+                    const pixelColumnIndex = beginningOfColumn + i
+                    const onScreenBorderX = (gpu.scrollY === pixelRowIndex || pixelRowIndex === gpu.scrollY + 144) && 
+                        (pixelColumnIndex > gpu.scrollX && pixelColumnIndex < gpu.scrollX + 160)
+                    const onScreenBorderY = (gpu.scrollX === pixelColumnIndex || gpu.scrollX + 160 === pixelColumnIndex) && 
+                        (pixelRowIndex > gpu.scrollY && pixelRowIndex < gpu.scrollY + 144)
                     if (onScreenBorderX || onScreenBorderY) {
                         canvasData[index] = 255
                         canvasData[index + 1] = 0
@@ -77,12 +78,12 @@ class Background extends React.Component<Props, State> {
                 })
             })
         })
-        const canvasWidth = widthInTiles * tileWidth
-        const canvasHeight = heightInTiles * tileHeight
+        const canvasWidthInPixels = widthInTiles * tileWidthInPixels
+        const canvasHeightInPixels = heightInTiles * tileHeightInPixels
         const data = new ImageData(
             Uint8ClampedArray.from(canvasData),
-            canvasWidth,
-            canvasHeight
+            canvasWidthInPixels,
+            canvasHeightInPixels
         )
 
 
