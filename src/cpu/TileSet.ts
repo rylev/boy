5e44085852cdd124a5bce07056e9a6a71fd2b42e
Ryan Levick <ryan.levick@gmail.com>
2017-05-28 12:22:55 +0200

    Show lines in tileset

--- src/cpu/TileSet.ts
+++ src/cpu/TileSet.ts
@@ -18,17 +18,27 @@ export namespace TileSet {
             const tileRow = Math.trunc(tileIndex / widthInTiles)
             const tileColumn = tileIndex % widthInTiles
             tile.forEach((row, rowIndex) => {
-                const beginningOfCanvasRow = ((tileRow * tileHeight) + rowIndex) * rowWidth
-                let index = beginningOfCanvasRow + (tileColumn * tileWidth * valuesPerPixel)
+                const pixelRowIndex = (tileRow * tileHeight) + rowIndex
+                const beginningOfCanvasRow = pixelRowIndex * rowWidth
+                const onTileRowBorder = pixelRowIndex % 8 === 0
+                const beginningOfColumn = tileColumn * tileWidth
+                let index = beginningOfCanvasRow + (beginningOfColumn * valuesPerPixel)
 
-                for (let pixel of row) {
-                    const color = gpu.valueToColor(pixel)
-                    canvasData[index] = color
-                    canvasData[index + 1] = color
-                    canvasData[index + 2] = color
+                row.forEach((pixel, i) => {
+                    const onTileColumnBorder = i % 8 === 0
+                    if (onTileRowBorder || onTileColumnBorder) {
+                        canvasData[index] = 0
+                        canvasData[index + 1] = 0
+                        canvasData[index + 2] = 255
+                    } else {
+                        const color = gpu.valueToColor(pixel)
+                        canvasData[index] = color
+                        canvasData[index + 1] = color
+                        canvasData[index + 2] = color
+                    }
                     canvasData[index + 3] = 255
                     index = index + valuesPerPixel
-                }
+                })
             })
         })
         const canvasWidth = widthInTiles * tileWidth
